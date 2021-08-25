eb4fc47f520af857d65d208d9cdb26ad52efe733
Matt Hamilton <matth651@gmail.com>
2021-08-25 18:42:06 -0400

    add email pass

--- bio/settings.py
+++ bio/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'matth651@gmail.com'
-EMAIL_HOST_PASSWORD = env('EMAIL_PASS')
+EMAIL_HOST_PASSWORD = 'crunkfish107'
 EMAIL_USE_TLS = True
 
 SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
